6bab904c02b0dd6a5561b7dfadce8eea3fe6b322
Vinay Arora <vinay.arora7171@hotmail.com>
2021-02-03 20:34:10 +0530

    Added Swagger To Api Project

--- YK.Item/Presentation/YK.Item.API/Startup.cs
+++ YK.Item/Presentation/YK.Item.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,18 @@ namespace YK.Item.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\YK.Item.API.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Invoicing, Time tracking, Expense Management",
+                });
+            });
+            #endregion
+
             services.AddControllers();
         }
 
@@ -41,6 +54,17 @@ namespace YK.Item.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ITEM");
+            });
+            #endregion
 
             app.UseEndpoints(endpoints =>
             {
