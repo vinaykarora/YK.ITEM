6a47f64d4ddcf481de4aeff3a7b4c09773fa1445
Vinay Arora <vinay.arora7171@hotmail.com>
2021-02-03 21:33:32 +0530

    Added Event Handler

---
+++ src/Core/YK.Item.Application/Handlers/ProductActivatedEmailNotificationHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using YK.Item.Application.Interfaces;
+using YK.Item.Domain.Events;
+
+namespace YK.Item.Application.Handlers
+{
+    public class ProductActivatedEmailNotificationHandler : INotificationHandler<ProductActivatedEvent>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public ProductActivatedEmailNotificationHandler(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        // configure a test email server to demo this works
+        // https://ardalis.com/configuring-a-local-test-email-server
+        public Task Handle(ProductActivatedEvent domainEvent, CancellationToken cancellationToken)
+        {
+            if (domainEvent is null)
+            {
+                throw new ArgumentNullException(nameof(domainEvent));
+            }
+
+            return _emailSender.SendEmailAsync("test@test.com", "test@test.com", $"{domainEvent.ActivedProduct.Name} was completed.", domainEvent.ActivedProduct.ToString());
+        }
+    }
+}
---
+++ src/Core/YK.Item.Application/Interfaces/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace YK.Item.Application.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string to, string from, string subject, string body);
+    }
+}
